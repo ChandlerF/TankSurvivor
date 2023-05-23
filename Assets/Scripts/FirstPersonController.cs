b1361193746a1165d34582d82e67a458b183385b
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2023-05-23 16:05:21 -0500

    Updated FPS Script to New Input
    Need to add a setting for mouse/joystick sensitivity

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 
 public class FirstPersonController : MonoBehaviour
 {
     public float walkingSpeed = 7.5f;
-    public float runningSpeed = 11.5f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
     public Camera playerCamera;
@@ -20,6 +20,9 @@ public class FirstPersonController : MonoBehaviour
 
     [HideInInspector]
     public bool canMove = true;
+
+    private bool _isJump = false;
+    private Vector2 _moveInput, _mouseInput;
 
     void Start()
     {
@@ -35,21 +38,22 @@ public class FirstPersonController : MonoBehaviour
         // We are grounded, so recalculate move direction based on axes
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
-        // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
-        {
-            moveDirection.y = jumpSpeed;
-        }
-        else
-        {
-            moveDirection.y = movementDirectionY;
-        }
+        float curSpeedX = canMove ? (walkingSpeed * _moveInput.y) : 0;
+        float curSpeedY = canMove ? (walkingSpeed * _moveInput.x) : 0;
+        float _movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+        moveDirection.y = _movementDirectionY;
+
+        if (_isJump && canMove && characterController.isGrounded)
+         {
+             moveDirection.y = jumpSpeed;
+            _isJump = false;
+         }
+         else
+         {
+             moveDirection.y = _movementDirectionY;
+         }
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
         // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
@@ -65,10 +69,27 @@ public class FirstPersonController : MonoBehaviour
         // Player and Camera rotation
         if (canMove)
         {
-            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX += -_mouseInput.y * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+            transform.rotation *= Quaternion.Euler(0, _mouseInput.x * lookSpeed, 0);
         }
+    }
+
+
+    private void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
+    }
+
+
+    private void OnJump()
+    {
+        _isJump = true;
+    }
+
+    private void OnLook(InputValue value)
+    {
+        _mouseInput = value.Get<Vector2>();
     }
 }
