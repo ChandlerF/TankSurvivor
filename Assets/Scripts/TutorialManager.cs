fe4fd27618d5216459731fac6131701009fd93cf
NBacon659 <104604606+NBacon659@users.noreply.github.com>
2023-05-29 12:30:01 -0700

    Added event again

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,39 +1,12 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TutorialManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] popUps;
+    [SerializeField] TMPro.TextMeshProUGUI text;
 
-    int popUpIndex;
-    bool isIntro;
-    private void Update()
-    {
-        for(int i = 0; i < popUps.Length; i++)
-        {
-            if( i == popUpIndex)
-            {
-                popUps[i].SetActive(true);
-            }
-            else
-            {
-                popUps[i].SetActive(false);
-            }
-        }
 
-        if(popUpIndex == 0)
-        {
-            if(isIntro)
-            {
-                StartCoroutine(Intro());
-            }
-        }
-    }
 
-    IEnumerator Intro()
-    {
-        isIntro = true;
-        yield return new WaitForSeconds(1);
-    }
+
 }
